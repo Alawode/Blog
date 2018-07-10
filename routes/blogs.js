a29cca627bde783f94bf2075a590b3c731a7c4fb
Emmanuel Alawode <emmanuelalawode99@gmail.com>
2018-07-10 15:51:44 -0400

    Update the log in and sign up pages

--- routes/blogs.js
+++ routes/blogs.js
@@ -3,6 +3,28 @@ var router = express.Router();
 var Blog = require('../models/blog');
 var middleware = require('../middleware');
 
+var multer = require('multer');
+var storage = multer.diskStorage({
+  filename: function(req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
+});
+var imageFilter = function (req, file, cb) {
+    // accept image files only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+        return cb(new Error('Only image files are allowed!'), false);
+    }
+    cb(null, true);
+};
+var upload = multer({ storage: storage, fileFilter: imageFilter})
+
+var cloudinary = require('cloudinary');
+cloudinary.config({
+  cloud_name: 'emmanuelalawode',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
 //====================
 // Index route
 router.get("/", function(req, res){
